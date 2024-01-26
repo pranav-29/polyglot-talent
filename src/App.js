fe817932a95701de086c4ef0206309323958b009
pranav-29 <pranavkumar679784@gmail.com>
2024-01-26 13:00:25 +0530

    Google Anaylatics Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 import Home from "./pages/Home.js";
+import React, {useEffect} from 'react';
 
 function App() {
+
+  //Google Analyatic
+  useEffect(() => {
+    window.gtag('config', 'G-MPHHGKLFKS');
+
+    return () => {};
+  }, []);
+//end
+
   return (
     <div className="App">
       <Home />
